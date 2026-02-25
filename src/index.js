8edbf0a3e36786ceeda1c16d2c22ec8c6161f131
Ziphezinhle Ntanzi <ntanziziphezinhle@gmail.com>
2026-02-25 11:33:49 +0200

    debugged forecast in js

--- src/index.js
+++ src/index.js
@@ -74,18 +74,20 @@ function displayForecast(response) {
     if (index < 5) {
       forecastHtml =
         forecastHtml +
-        ` <div class="weather-forecast-date"> 
-      <div class="weather-forecast-day">${formatDay(day.time)}</div> 
-      <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
-      <div class="weather-forecast-temperatures"> 
-      <div class="weather-forecast-temperature"> <strong>${Math.round(
-        day.temperature.maximum
-      )}º</strong></div> 
-      <div class="weather-forecast-temperature">${Math.round(
-        day.temperature.minimum
-      )}º</div> 
-      </div>
-      `;
+        `
+         <div class="weather-forecast-date"> 
+            <div class="weather-forecast-day">${formatDay(day.time)}</div> 
+            <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+            <div class="weather-forecast-temperatures"> 
+            <div class="weather-forecast-temperature"> <strong>${Math.round(
+              day.temperature.maximum
+            )}º</strong></div> 
+            <div class="weather-forecast-temperature">${Math.round(
+              day.temperature.minimum
+            )}º</div> 
+            </div>
+          </div>
+        `;
     }
   });
   let forecastElement = document.querySelector("#forecast");
